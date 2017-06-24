bdee432d69dad35b29046be2553ccb0f98074921
Peter Romfeld <peter@exiconglobal.com>
2017-06-24 17:31:50 +0800

    Use destructuring to only select relevant keys

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ program
   .option('-a, --app-id <app-id>', 'the iTunes "bundleId" of the app, for example com.midasplayer.apps.candycrushsaga for Candy Crush Saga. Either this or the id should be provided.')
   .option('-c, --country <cc>', ' the two letter country code to get the app details from. Defaults to us. Note this also affects the language of the data.')
 
-  .action(options => {
+  .action(args => {
+    const options = (({ id, appId, country }) => ({ id, appId, country }))(args)
     itc.app(options)
-    .then(handle_result, handle_error)
+      .then(handle_result, handle_error)
   })
 
 program
@@ -26,22 +27,27 @@ program
   .option('-c, --country <cc>', 'the two letter country code to get the list from. Defaults to us.')
   .option('-n, --num <n>', 'the amount of elements to retrieve. Defaults to 50, maximum allowed is 200.', parseInt)
 
-  .action(options => {
+  .action(args => {
+    const options = (({ collection, category, country, num }) => ({ collection, category, country, num }))(args)
+    options.collection = itc.collection[options.collection]
+    options.category = itc.category[options.category]
     itc.list(options)
-    .then(handle_result, handle_error)
+      .then(handle_result, handle_error)
   })
 
 program
   .command('search')
   .description('Retrieves a list of apps that results of searching by the given term.')
   .option('-t, --term <term>', 'the term to search by.')
-  .option('-d, --device <DEVICE>', 'the device to filter for. Defaults to store.device.ALL, available options are ALL, MAC, IPAD.')
+  .option('-d, --device <DEVICE>', 'the device to filter for. Defaults to ALL, available options are ALL, MAC, IPAD.')
   .option('-n, --num <n>', 'the amount of elements to retrieve. Defaults to 50, maximum allowed is 200.', parseInt)
   .option('-c, --country <cc>', 'the two letter country code to get the similar apps from. Defaults to us.')
 
-  .action(options => {
+  .action(args => {
+    const options = (({ term, device, num, country }) => ({ term, device, num, country }))(args)
+    options.device = itc.device[options.device]
     itc.search(options)
-    .then(handle_result, handle_error)
+      .then(handle_result, handle_error)
   })
 
 program
@@ -49,9 +55,10 @@ program
   .description('Given a string returns up to 50 suggestions to complete a search query term.')
   .option('-t, --term <term>', 'the term to search by.')
 
-  .action(options => {
+  .action(args => {
+    const options = (({ term }) => ({ term }))(args)
     itc.suggest(options)
-    .then(handle_result, handle_error)
+      .then(handle_result, handle_error)
   })
 
 program
@@ -63,9 +70,11 @@ program
   .option('-p, --page <n>', 'the review page number to retrieve. Defaults to 1, maximum allowed is 10.', parseInt)
   .option('-s, --sort <SORT>', 'the review sort order. Defaults to store.sort.RECENT, available options are RECENT and HELPFUL.')
 
-  .action(options => {
+  .action(args => {
+    const options = (({ id, appId, country, page, sort }) => ({ id, appId, country, page, sort }))(args)
+    options.sort = itc.sort[options.sort]
     itc.reviews(options)
-    .then(handle_result, handle_error)
+      .then(handle_result, handle_error)
   })
 
 program
@@ -75,9 +84,10 @@ program
   .option('-a, --app-id <lang>', 'the iTunes "bundleId" of the app, for example com.midasplayer.apps.candycrushsaga for Candy Crush Saga. Either this or the id should be provided.')
   .option('-c, --country <cc>', 'the two letter country code to get the similar apps from. Defaults to us.')
 
-  .action(options => {
+  .action(args => {
+    const options = (({ id, appId, country }) => ({ id, appId, country }))(args)
     itc.similar(options)
-    .then(handle_result, handle_error)
+      .then(handle_result, handle_error)
   })
 
 function handle_result(res){
